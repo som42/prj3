35c0af99f8542bd92e71f593afcb9e486f663d77
som42 <zzzoo42@naver.com>
2024-01-08 11:56:32 +0900

    initial

---
+++ src/main/java/com/example/prj3/Prj3Application.java
@@ -0,0 +1,13 @@
+package com.example.prj3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj3/Prj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
